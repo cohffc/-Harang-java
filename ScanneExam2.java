3342f95a1d381d0d20d0249448157f7302bff965
조하랑 <111717461+cohffc@users.noreply.github.com>
2022-10-31 14:14:39 +0900

    Update ScanneExam2.java

--- ScanneExam2.java
+++ ScanneExam2.java
@@ -10,5 +10,13 @@ public class ScanneExam2 {
 		
 		System.out.println("num 값:" + num);
 		System.out.println("str 값:" + str);
+		
+		
+		System.out.print("정수 값 입력:");
+		int num1 = scanner.nextInt();
+		System.out.print("실수 값 입력:");
+		double num2 = scanner.nextDouble();
+		System.out.println("정수 값:" + num1 + ("\n실수 값 입력 :") + num2);
+		
 	}
 }
