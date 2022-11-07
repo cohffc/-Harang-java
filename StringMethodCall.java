8fd3bdacb77c01f653eee660b7e47a7cf6b6a714
조하랑 <111717461+cohffc@users.noreply.github.com>
2022-11-07 15:18:24 +0900

    Update StringMethodCall.java

--- StringMethodCall.java
+++ StringMethodCall.java
@@ -10,12 +10,12 @@ class StringMethodEx
 	 }
 	 void inputString(int sta, int end) 
 	 {
-	    System.out.println("substring: " + str.substring(sta, end));
+	    System.out.println("String클래스에는 "+ sta + "번째" + "~" + end + "번째 문자 변환" + str.substring(sta, end));
 	    
 	 }
 	 void inputString(int n) 
 	 {
-	    System.out.println("charAt: " + str.charAt(n));
+	    System.out.println("str 문자열"+ n + "번째 문자 반환: " + str.charAt(n));
 	 }
 }
 
@@ -36,7 +36,12 @@ public class StringMethodCall
 	      stringmethodex.inputString(scanner.nextInt());
 
 	      System.out.println("입력 문자열 특정부분의 문자 반환");
+	      System.out.println("반환 문자열의 시작위치와 종료위치 입력");
 	      stringmethodex.inputString(scanner.nextInt(), scanner.nextInt());
+	      
+	      
+	      System.out.println("");
+	      System.out.println("String클래스에는 그밖에도 문자열관련 메소드가 있음.");
 
 
 
